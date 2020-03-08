f5d3f9692922eb01d12e01c4ec6527ba3def66ec
Nikolai Vetrov <nvetrov@gmail.com>
2020-03-08 12:39:41 +0300

    django-allauth

--- common/urls.py
+++ common/urls.py
@@ -2,12 +2,15 @@ from common.views import index, RegisterView, CreateUserProfile
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.urls import path
+from allauth.account.views import login, logout
 
 app_name = 'common'
 urlpatterns = [
     path('', index, name='index'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    # path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(
         template_name='register.html',
         success_url=reverse_lazy('common:profile-create')
